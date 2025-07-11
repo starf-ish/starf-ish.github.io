2a08354b4ea22188c5fe169d5864ab6c65b099c6
PhoenixAI0 <richard.miao.sg@gmail.com>
2025-07-11 15:41:27 +0800

    Set up GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/starf-ish.github.io/', // <-- change this!
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
