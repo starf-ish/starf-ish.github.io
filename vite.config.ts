0f013d44e661d95236bd4cd0e4bd382517183d2c
PhoenixAI0 <richard.miao.sg@gmail.com>
2025-07-11 15:42:39 +0800

    minor changes again

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/starf-ish.github.io/', // <-- change this!
+  base: '/starf-ish.github.io/',
   plugins: [react()],
 })
